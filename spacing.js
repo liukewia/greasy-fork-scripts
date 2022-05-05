6decca09e6044dd80f33044292416918cf39f90f
finn <liukewia@gmail.com>
2022-05-05 17:53:46 +0800

    feat: add spacing-js script

---
+++ spacing.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Spacing Js
+// @namespace    https://spacingjs.com/
+// @version      0.1
+// @description  Spacing Js
+// @author       You
+// @match        *://*/*
+// @require      https://unpkg.com/spacingjs
+// @icon         https://lh3.googleusercontent.com/Z21RzHwgZJ25br-OdZV0ME4zjkrEPnsoGm8DGTbgk7x5cP-wpwav91fYtcmchLx_pHonfD_AFGaBj2yAKIvKqFppIA=w128-h128-e365-rj-sc0x00ffffff
+// @grant        none
+// @updated-at  2022/5/5
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // Your code here...
+})();
