a9d7d13949b0aa0956cbdd7e8f7d29f7cf38cdd6
Finn <liukewia@gmail.com>
2023-01-26 14:56:03 +0800

    feat: update

--- v2ex-dark-mode.js
+++ v2ex-dark-mode.js
@@ -4,57 +4,73 @@
 // @namespace   http://tampermonkey.net/
 // @author      liukewia
 // @icon        https://www.v2ex.com/static/favicon.ico
-// @version     1.0
+// @version     1.0.1
 // @match       https://v2ex.com/*
 // @match       https://www.v2ex.com/*
 // @grant       none
-// @updated-at  2023/01/25
+// @updated-at  2023/01/26
 // ==/UserScript==
 
-// 用户系统是否偏好暗黑
-const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+(() => {
+  "use strict";
 
-const getToggleBtn = () => document.querySelector("a.light-toggle");
-
-const getIsDocumentLoaded = getToggleBtn;
-
-// 当前网页是否为黑
-const isDark = () => {
-  const wrapper = document.getElementById("Wrapper");
-  if (!wrapper) {
-    return false;
-  }
-
-  return wrapper.classList.contains("Night");
-};
-
-const toggleTheme = () => {
-  const btn = getToggleBtn();
-  if (!btn || !(btn instanceof HTMLElement)) {
+  if (!window.matchMedia) {
+    console.warn("不支持 matchMedia API");
     return;
   }
-  btn.click();
-};
 
-const work = () => {
-  if (!getIsDocumentLoaded()) {
-    return false;
-  }
-  if (isDark() && !prefersDark) {
-    toggleTheme();
-  }
-  if (!isDark() && prefersDark) {
-    toggleTheme();
-  }
-  return true;
-};
+  const toggleBtn = document.querySelector(".light-toggle");
 
-const poll = () =>
-  setTimeout(() => {
-    const result = work();
-    if (!result) {
-      poll();
+  if (!toggleBtn) {
+    console.warn("找不到 toggle btn");
+    return;
+  }
+
+  const toggleLink = toggleBtn.getAttribute("href");
+  if (!toggleLink) {
+    console.warn("找不到 toggle link");
+    return;
+  }
+
+  const mediaQueryLight = window.matchMedia("(prefers-color-scheme: light)");
+  const isLightPreferred = mediaQueryLight.matches;
+
+  // 用户系统是否偏好暗黑
+  const mediaQueryDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+  const isDarkPreferred = mediaQueryDark.matches;
+
+  // 当前网页是否为黑
+  const isDark = (() => {
+    const wrapper = document.getElementById("Wrapper");
+    if (!wrapper) {
+      return false;
     }
-  }, 100);
 
-poll();
+    return wrapper.classList.contains("Night");
+  })();
+
+  const toggle = () => {
+    window.location.replace(toggleLink);
+  };
+
+  if (isLightPreferred && isDark) {
+    toggle();
+  }
+
+  mediaQueryLight.addEventListener("change", () => {
+    if (isDark) {
+      toggle();
+    }
+  });
+
+  if (isDarkPreferred && !isDark) {
+    toggle();
+  }
+
+  mediaQueryDark.addEventListener("change", () => {
+    if (!isDark) {
+      toggle();
+    }
+  });
+})();
